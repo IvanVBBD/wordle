941a33d7cbd7464dd94bfaabe1b57c19d7f5c6b0
Kayden Kara <kayden@bbd.co.za>
2023-05-08 08:25:58 +0200

    Fixed escaped characters in sql query

--- routes/Utility/authUtils.js
+++ routes/Utility/authUtils.js
@@ -5,7 +5,8 @@ const DB = require("./dbUtils")
 
 async function checkUserProfile(email){
     console.log("check if user : " + email + " exists");
-    const statement = "SELECT * FROM USERS WHERE user_email = " + email;
+    const statement = "SELECT * FROM USERS WHERE user_email = " + "\'" + email + "\'";
+    console.log(statement);
     let result = await DB.executeQuery(statement);
 }
 
