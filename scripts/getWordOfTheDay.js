6cfc140f8eaf7b159e3363431a4babd9dc8a35df
Manuel Nunes <122288176+Manuel-Nunes@users.noreply.github.com>
2023-05-18 01:22:37 +0200

    Integrated from RapidDev

--- scripts/getWordOfTheDay.js
+++ scripts/getWordOfTheDay.js
@@ -1,12 +1,16 @@
+import { UIConstants } from './gameUIConstants.js';
 /**
  * Returns the queried word of the day
  * @returns @type {string}
  */
+
 export default async function getWordOfTheDay(){
-  //Hit endpoint here
+  if (UIConstants.APILocalMode)
+    return 'Plant';
+
   const baseUrl = window.location.href.split('/').slice(0, 3).join('/');
   const request = await fetch (`${baseUrl}/Game/GetChallenge`);
-  const data = await request.json()
-  console.log(data.word)
+  const data = await request.json();
+  console.log(data.word);
   return data.word;
 }
