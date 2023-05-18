5b863503033b9a2f282a232c840563c5b971e73e
JacquesGriesel <Jacques.Griesel@bbd.co.za>
2023-05-18 13:24:32 +0200

    Leaderboard

--- scripts/highscorePage.js
+++ scripts/highscorePage.js
@@ -1,17 +1,29 @@
 const scoreBoard = document.getElementById("scoreBoard")
 const backButton = document.getElementById("backButton")
+const UserScoreRow = document.getElementById("userRow")
 const baseUrl = window.location.href.split("/").slice(0, 3).join("/");
 
 const getScore =  async () => {
     console.log(baseUrl);
+    let counter = 0;
     let data = await fetch(baseUrl + "/" + "Highscore" + "/" + "scores");
     data = await data.json();
     data = data.highscore
     data.sort((a, b) => b.score - a.score);
     data.map(x=>{
-        const newListItem = document.createElement('li');
-        newListItem.textContent = x.user_email + ' - ' + x.score
-        scoreBoard.appendChild(newListItem);
+        const newListRow= document.createElement('tr');
+        const newRankItem = document.createElement('td');
+        const newNameItem = document.createElement('td');
+        const newTimeItem = document.createElement('td');
+        counter++
+        //newListItem.textContent = x.user_email + ' - ' + x.score
+        newRankItem.textContent = counter.toString();
+        newNameItem.textContent = x.user_email ;
+        newTimeItem.textContent = "Time value" //change to time attribute later
+        newListRow.appendChild(newRankItem);
+        newListRow.appendChild(newNameItem);
+        newListRow.appendChild(newTimeItem);
+        scoreBoard.appendChild(newListRow);
     })
     
 
@@ -19,8 +31,8 @@ const getScore =  async () => {
 
 getScore();
 
-backButton.onclick = function() {
-    console.log("we in here");  
-    window.location.href = baseUrl + "/Game";
-  };
+//backButton.onclick = function() {
+//  console.log("we in here");  
+//    window.location.href = baseUrl + "/Game";
+// };
 
